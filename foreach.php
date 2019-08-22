db9340933d9005458e63a9c1245bf2ed154154ec
matt0418 <mfeldma7@gmail.com>
2019-08-22 16:31:45 -0400

    foreacg

---
+++ foreach.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <?php 
+    
+        $practice = [1,2,3,4,5,6];
+
+        foreach($practice as $number) {
+            echo "<h1>$number</h1>";
+        }
+
+        print_r($practice);
+    
+    ?>
+
+
+</body>
+</html>
