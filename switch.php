c46aa03406c3a112150f641d07ac0ed6d51c9abe
matt0418 <mfeldma7@gmail.com>
2019-08-22 16:17:39 -0400

    completed switches

---
+++ switch.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php 
+    
+    $number = 100;
+
+    switch($number) {
+        case 34:
+            echo "is it 34";
+            break;
+        case 37:
+            echo "it is 37";
+            break;
+        case 22:
+            echo "it is 22";
+            break;
+        case 10:
+            echo "it is 10";
+            break;
+
+        default:
+            echo "Not working";
+    }
+
+    
+    
+    ?>
+
+
+</body>
+</html>
