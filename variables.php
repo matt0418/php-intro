656ab6a00038015eaef7b9274b4be722c6b91c00
matt0418 <mfeldma7@gmail.com>
2019-08-21 21:37:41 -0400

    variables completed

---
+++ variables.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php 
+    
+        $name = "Matthew";
+        $number = 100;
+        $Number_List = 100;
+
+        $thing = $number - 100;
+
+        echo $thing;
+
+        echo "<br>";
+
+        echo $name . " " . $number;
+
+
+        echo "<p>$name</p>";
+
+    ?>
+    
+</body>
+</html>
