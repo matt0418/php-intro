f6182c93bbb04562702bac4030de7ea3aa78e277
matt0418 <mfeldma7@gmail.com>
2019-08-21 21:51:55 -0400

    arrays

---
+++ arrays.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php 
+
+        $numberList = array();
+        // Like JavaScript V 
+        $betterWay = [23,59,54,23,1];
+
+        echo $betterWay[1];
+
+        print_r($betterWay);
+
+    ?>
+
+
+</body>
+</html>
