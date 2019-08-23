2991256182f31664c391c26c6d89359aaad40c47
matt0418 <mfeldma7@gmail.com>
2019-08-23 12:56:37 -0400

    some more files

---
+++ array_funcs.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <?php 
+    
+    $thing = [4,2,3,1,5,6,7,8,9,10];
+
+    echo sizeof($thing);
+
+    echo "<br>";
+
+    echo max($thing);
+
+    sort($thing);
+
+    print_r($thing);
+    
+    ?>
+
+</body>
+</html>
